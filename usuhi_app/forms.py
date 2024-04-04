8266b37971b50e657d9759aa730b96a7fef87070
Beknur Arystan <Beknurarystan@gmail.com>
2024-04-04 15:54:11 +0500

    First commit

--- usuhi_app/forms.py
+++ usuhi_app/forms.py
@@ -4,14 +4,14 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Food
 from django import forms
 
+
 class NewsUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
 
-
 class AddFoodForm(forms.ModelForm):
     class Meta:
         model = Food
-        fields = ('name', 'category','description', 'image', 'price', 'count')
+        fields = ('name', 'category', 'description', 'image', 'price', 'count')
